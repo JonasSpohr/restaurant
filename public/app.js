78595c770b0fcd04a6b0c40099a8964f29c25ffe
Jonas Spohr da Silva <jonass@dbserver.com.br>
2016-12-08 14:49:09 -0200

    .

--- public/app.js
+++ public/app.js
@@ -1,2 +1,2 @@
-var _myApp = angular.module('app', ['ngRoute', 'ngResource', 'angular-md5', 'ngStorage', 'ngMask', 'ui.calendar', 'ui.bootstrap']);
+var _myApp = angular.module('app', ['ngRoute', 'ngResource', 'angular-md5', 'ngStorage', 'ngMask', 'ui.calendar', 'ui.bootstrap', 'google.places']);
 angular.module('app.controllers', []);
